1a5c51f9c83fe5ca7c67bbc2ee0204f3f338a62f
anthony peter <anthonypeter1234@gmail.com>
2023-02-20 14:02:48 -0800

    var

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints alphabets
+ * Description: prints trings of alphabets
+ * Return: 0
+ */
+int main(void)
+{
+int y;
+for (y = 0; y <= 9; y++)
+{
+putchar('0' + y);
+putchar(',')
+putchar(' ')
+}
+putchar('\n');
+return (0);
+}
