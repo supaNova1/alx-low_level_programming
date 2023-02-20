6fad4ac8e1c72cc5ce181a7b0bdc37bf00334a8a
anthony peter <anthonypeter1234@gmail.com>
2023-02-20 12:55:07 -0800

    varr

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints alphabets
+ * Description: prints trings of alphabets
+ * Return: 0
+ */
+int main(void)
+{
+int y;
+for (y = 0; y <= 9; y++)
+{
+printf("%d\n", y);
+}
+return (0);
+}
