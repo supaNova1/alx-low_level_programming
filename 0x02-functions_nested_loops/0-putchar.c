ea25a66023f474d293015378d59faab85fe69072
anthony peter <anthonypeter1234@gmail.com>
2023-02-22 10:52:04 -0800

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - print _putchar
+ *
+ *Return: 0
+*/
+int putchar()
+{
+  for(int c = 1; c <= 1; c++)
+    {
+      _putchar(_putchar);
+      _putchar('\n');
+      return (0);
+    }
+}
