0cd8c37de53df63f7bdfdb9f7d830d512a8b20f7
supaNova1 <anthonypeter1234@gmail.com>
2024-02-08 21:09:35 +0100

    recheck

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,30 @@
-#include<stdio.h>
-#include"main.h"
-/*
- * binary_to_unit converts a binary number to an unsigned int.
- *@b pointer to a string containing the binary number
- *Return unsigned int with decimal value of the binary number, 0 if error
-*/
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *binary_to unit - converts a binary number to unsigned interger number.
+ *@b: pointer to a string containing the binary number.
+ *
+ *Return: unsigned integer with decimal number of binary value or 0 if error.
+ */
 unsigned int binary_to_unit(const char *b)
 {
-  int i;
-  unsigned int number;
-  number = 0;
-  if (!b)
-    return(0);
-  for (i=0;b[i]!='\0';i++)
-    {
-      if (b[i]!='0'&& b[i]!='1')
-	return(0);
-    }
-  for (i=0; b[i]!='\0';i++)
-    {
-      number <<= 1;
-      if (b[i] == '1')
-	number += 1;
-    }
-  return (number);
+	inti;
+	unsigned int number;
+
+	number = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		number <<= 1;
+		if (b[i] == '1')
+			number += 1;
+	}
+	return (number);
 }
